a2c71cba91f03c8503af6758fbec55c844402400
JiteshNikale <nikale.jitesh@gmail.com>
2023-02-12 23:00:11 +0530

    service registry in eureka

--- src/main/java/com/fiserv/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/fiserv/employeeservice/EmployeeServiceApplication.java
@@ -3,6 +3,7 @@ package com.fiserv.employeeservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	/*@Bean
